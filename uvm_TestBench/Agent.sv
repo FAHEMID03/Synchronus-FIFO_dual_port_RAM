10167875887905930465e685bffa9511795f2a88
SK ABDUL FAHEMID <53863051+FAHEMID03@users.noreply.github.com>
2026-07-10 11:50:04 +0530

    Add sync_fifo_read_agent class to Agent.sv

--- uvm_TestBench/Agent.sv
+++ uvm_TestBench/Agent.sv
@@ -1,4 +1,4 @@
-define SYNC_FIFO_WRITE_AGENT_SV
+define SYNC_FIFO_AGENT_SV
 
 class sync_fifo_write_agent extends uvm_agent;
 
@@ -60,6 +60,141 @@ class sync_fifo_write_agent extends uvm_agent;
       super.connect_phase(phase);
 
       drv.seq_item_port.connect(seqr.seq_item_export);
+
+   endfunction
+
+endclass
+
+class sync_fifo_read_agent extends uvm_agent;
+
+   //--------------------------------------------
+   // Factory Registration
+   //--------------------------------------------
+
+   `uvm_component_utils(sync_fifo_read_agent)
+
+   //--------------------------------------------
+   // Component Handles
+   //--------------------------------------------
+
+   sync_fifo_read_sequencer seqr;
+
+   sync_fifo_read_driver drv;
+
+   sync_fifo_read_monitor mon;
+
+   //--------------------------------------------
+   // Constructor
+   //--------------------------------------------
+
+   function new(string name="sync_fifo_read_agent",
+                uvm_component parent);
+
+      super.new(name,parent);
+
+   endfunction
+
+   //--------------------------------------------
+   // Build Phase
+   //--------------------------------------------
+
+   function void build_phase(uvm_phase phase);
+
+      super.build_phase(phase);
+
+      seqr =
+      sync_fifo_read_sequencer::
+      type_id::create("seqr",this);
+
+      drv =
+      sync_fifo_read_driver::
+      type_id::create("drv",this);
+
+      mon =
+      sync_fifo_read_monitor::
+      type_id::create("mon",this);
+
+   endfunction
+
+   //--------------------------------------------
+   // Connect Phase
+   //--------------------------------------------
+
+   function void connect_phase(uvm_phase phase);
+
+      super.connect_phase(phase);
+
+      drv.seq_item_port.connect
+      (
+      seqr.seq_item_export
+      );
+
+   endfunction
+
+endclass
+class sync_fifo_read_agent extends uvm_agent;
+
+   //--------------------------------------------
+   // Factory Registration
+   //--------------------------------------------
+
+   `uvm_component_utils(sync_fifo_read_agent)
+
+   //--------------------------------------------
+   // Component Handles
+   //--------------------------------------------
+
+   sync_fifo_read_sequencer seqr;
+
+   sync_fifo_read_driver drv;
+
+   sync_fifo_read_monitor mon;
+
+   //--------------------------------------------
+   // Constructor
+   //--------------------------------------------
+
+   function new(string name="sync_fifo_read_agent",
+                uvm_component parent);
+
+      super.new(name,parent);
+
+   endfunction
+
+   //--------------------------------------------
+   // Build Phase
+   //--------------------------------------------
+
+   function void build_phase(uvm_phase phase);
+
+      super.build_phase(phase);
+
+      seqr =
+      sync_fifo_read_sequencer::
+      type_id::create("seqr",this);
+
+      drv =
+      sync_fifo_read_driver::
+      type_id::create("drv",this);
+
+      mon =
+      sync_fifo_read_monitor::
+      type_id::create("mon",this);
+
+   endfunction
+
+   //--------------------------------------------
+   // Connect Phase
+   //--------------------------------------------
+
+   function void connect_phase(uvm_phase phase);
+
+      super.connect_phase(phase);
+
+      drv.seq_item_port.connect
+      (
+      seqr.seq_item_export
+      );
 
    endfunction
 
